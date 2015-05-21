380fdb2ee2c5cb57006795ce0e56105f3d96d564
KristoferSjhlm <kristofer.sjoholm@svt.se>
2015-05-21 09:39:59 +0200

    Update cachematrix.R
    Done the programming for the Programming Assesment 2. Ready for peer evaluation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The "makeCacheMatrix" creates matrix object that can be stored and fetch from the cache
+## using the following functions. In this case we can get the cached inversed matrix withou calculations
 
-## Write a short comment describing this function
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inversed matrix
+## 4. set the value of the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+          m <- NULL
+          set <- function(y) {
+                      x <<- y
+                      m <<- NULL
+          }
+          get <- function() x
+          setmatrix <- function(matrix) m <<- matrix # assigns the matrix to be stored in the cache
+          getmatrix <- function() m
+          
+          # returns the functions as a list
+          list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## This function checkes if the inverse matrix already has been stores in th cache
+## If it has, it fetches the inverse matrix from the cache.
+## If it's a new matrix that is passed in to the function, it computes the inverse matrix 
+## using the solve() function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          
+          m <- x$getmatrix() # get the matrix object from the getmatrix function
+          
+          if(!is.null(m)) { # checks if the calculation is stores in cache 
+                    message("getting cached data")
+                    return(m) # return the cached matrix that is the inverse of 'x'
+          }
+          data <- x$get() # if not, get the data
+          m <- solve(data, ...) # calculate the inverse matrix using solve() 
+          x$setmatrix(m) # store the new data in in the cache 
+          m # Return the new matrix that is the inverse of 'x'
 }
+
+## To try the solution try for example:
+## > m1 <- makeCacheMatrix(matrix(21:24, nrow=2, ncol=2))
+## > cacheSolve(m1)
